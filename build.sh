9fa57a3a82f0dc1693de8a9358d94d4ec35e5ba4
Lyndon <snakeliwei@gmail.com>
2016-10-09 16:20:03 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 set -e
 
-if [ "${CA_CERT}" == "**None**" ]; then
-    echo "Please specify CA_CERT"
+if [ "${DOMAIN}" ]; then
+    echo "Please specify DOMAIN"
     exit 1
 fi
 
-echo -e "${CA_CERT}" > /ngrok/assets/client/tls/ngrokroot.crt
+if [ "${DOMAIN}" ]; then
+    echo "Please specify DOMAIN"
+    exit 1
+fi
 
 if [ ! -f /ngrok/bin/ngrokd ]; then
     echo "=> Compiling ngrok binary files"
-    cd /ngrok; make release-server release-client
+    cd /ngrok
+    openssl genrsa -out rootCA.key 2048
+    openssl req -x509 -new -nodes -key rootCA.key -subj "/CN=$DOMAIN" -days 50000 -out rootCA.pem
+    openssl genrsa -out device.key 2048
+    openssl req -new -key device.key -subj "/CN=$DOMAIN" -out device.csr
+    openssl x509 -req -in device.csr -CA rootCA.pem -CAkey rootCA.key -CAcreateserial -out device.crt -days 5000
+    cp rootCA.pem assets/client/tls/ngrokroot.crt
+    cp device.crt assets/server/tls/snakeoil.crt 
+    cp device.key assets/server/tls/snakeoil.key
+    make release-server release-client
     echo "=> Successfully built the binaries"
 fi
